ca2f8fe2fef8dbb32343c564dc9effa23e809c5c
Nathália Oliveira <nathaliaolisantana@gmail.com>
2025-04-11 12:17:05 -0300

    using @lazy annotation

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -2,12 +2,16 @@ package main;
 
 import config.ProjectConfig;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import services.CommentService;
 
 public class Main {
     public static void main(String[] args) {
 
-        // this app creates the Spring context, but it doesn't use the CommentService bean anywhere
         var c = new AnnotationConfigApplicationContext(ProjectConfig.class);
+
+        System.out.println("Before retrieving the CommentService");
+        var service = c.getBean(CommentService.class);
+        System.out.println("After retrieving the CommentService");
 
     }
 }
